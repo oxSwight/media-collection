fdfc0ecd2c48dd0166aa88a9671b239e13814da9
IgorZ <igorzomm3r@gmail.com>
2025-12-05 12:17:26 +0100

    fix theme button

--- src/includes/header.php
+++ src/includes/header.php
@@ -24,7 +24,7 @@ if ($myId) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="<?= htmlspecialchars(csrf_token()) ?>">
     <title><?= htmlspecialchars(t('site.title')) ?></title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css?v=2.0">
     <script>
         // Тема (светлая/темная)
         (function() {
